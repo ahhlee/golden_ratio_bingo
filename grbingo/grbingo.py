d56f46b7c16914643625e928b5ee706269a7eff9
ahhlee <20246529+ahhlee@users.noreply.github.com>
2021-02-22 17:53:19 -0800

    Add files via upload

---
+++ grbingo/grbingo.py
@@ -0,0 +1,66 @@
+#Python 3
+
+import random
+import requests
+import pandas as pd
+from io import StringIO
+#from prettytable import PrettyTable
+from tabulate import tabulate
+
+
+
+freespace='Free Space'
+
+# Read the CSV into pandas data frame (df)
+#df = pd.read_csv('grbingo.csv', header=None)
+
+url= requests.get('https://docs.google.com/spreadsheets/d/' + 
+                   '1BezMJn85XcywGSUkN_KO4_OuTh-8xrtCV6wXwIYOcrs' +
+                   '/export?gid=0&format=csv')
+csv_raw = StringIO(url.text)
+df = pd.read_csv(csv_raw, header=None)
+
+#Make list of 24 randomly generated numbers
+numlist = []
+while len(numlist)<25:
+     r=random.randint(0,len(df)-1)
+     if r not in numlist: numlist.append(r)
+          
+#Make list of bingo options
+bingolist = []
+for i in range(len(numlist)):
+    options=df.iloc[i][0]    
+    if i == 12:
+      bingolist.append(freespace)
+    else:
+      bingolist.append(options)
+row1=bingolist[0:5]
+print(row1)
+row2=bingolist[5:10]
+row3=bingolist[10:15]
+row4=bingolist[15:20]
+row5=bingolist[20:25]
+
+table=[row1,row2,row3,row4,row5]
+
+bingo=tabulate(table, headers=["B","I","N","G","O"], tablefmt="fancy_grid")
+
+
+
+
+     
+#bingo = PrettyTable(align='c', valign='m', hrrules=1, header=True, border=True)
+##Add the random prompts to the bingo table
+#bingo.field_names = ["B","I","N","G", "O"]
+#bingo.add_row(row1)
+#bingo.add_row(row2)
+#bingo.add_row(row3)
+#bingo.add_row(row4)
+#bingo.add_row(row5)
+
+
+#print(bingo.get_html_string(format=False))
+
+##Print output
+print(bingo)
+
